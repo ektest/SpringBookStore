b24cc498bc21c5e1a095845dc4358d8b1a8da0be
Emre <ekocausa@gmail.com>
2015-10-11 22:36:30 -0700

    Spring wiring and lazy-init, scope (Singelton or Stateful Beans)

--- src/com/emrekoca/avalon/services/PurchasingServiceImpl.java
+++ src/com/emrekoca/avalon/services/PurchasingServiceImpl.java
@@ -2,12 +2,19 @@ package com.emrekoca.avalon.services;
 
 import com.emrekoca.avalon.domain.Book;
 
-public class PurchasingServiceImpl implements PurchasingService
-{
+public class PurchasingServiceImpl implements PurchasingService {
 	private AccountsService accounts;
 	private BookService books;
 
+	public PurchasingServiceImpl() {
 
+	}
+
+	// Wiring via contructor
+	public PurchasingServiceImpl(AccountsService accountsService, BookService booksService) {
+		this.accounts = accountsService;
+		this.books = booksService;
+	}
 
 	// Wiring via property
 	public void setAccountService(AccountsService accounts) {
@@ -18,12 +25,11 @@ public class PurchasingServiceImpl implements PurchasingService
 		this.books = books;
 	}
 
-	public void buyBook(String isbn)
-	{
+	public void buyBook(String isbn) {
 		// find the correct book
 		Book requiredBook = books.getBookByIsbn(isbn);
 
 		// now raise the invoice
-		accounts.raiseInvoice(requiredBook); 
+		accounts.raiseInvoice(requiredBook);
 	}
 }
