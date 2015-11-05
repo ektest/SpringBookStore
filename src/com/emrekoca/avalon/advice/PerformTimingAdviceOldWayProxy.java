5aea265348ec5032f6a7addd1830ba37b1f2771f
Emre <ekocausa@gmail.com>
2015-11-05 15:06:29 -0800

    AspectJ approach

---
+++ src/com/emrekoca/avalon/advice/PerformTimingAdviceOldWayProxy.java
@@ -0,0 +1,21 @@
+package com.emrekoca.avalon.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class PerformTimingAdviceOldWayProxy implements MethodInterceptor {
+	@Override
+	public Object invoke(MethodInvocation method) throws Throwable {
+		long startTime = System.nanoTime();
+		try {
+			Object returnedValue = method.proceed();
+			return returnedValue;
+		} finally {
+			long endTime = System.nanoTime();
+			double timeTaken = (double)(endTime - startTime) / 1000000000.0 ;
+			System.out.println("The method " 
+					+ method.getMethod().getName() 
+					+ " took " + timeTaken + " seconds");
+		}
+	}
+}
