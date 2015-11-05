d3d4ee6c6c2337a467c22c60d66ddb4c4de6215b
Emre <ekocausa@gmail.com>
2015-11-05 15:08:05 -0800

    Backing up AspectJ approach before moving to Annotation approach

---
+++ src/com/emrekoca/avalon/advice/PerformTimingAdviceAspectJ.java
@@ -0,0 +1,26 @@
+package com.emrekoca.avalon.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformTimingAdviceAspectJ {
+	public Object performTimingMeasurement(ProceedingJoinPoint method) throws Throwable {
+		long startTime = System.nanoTime();
+		try {
+			Object returnedValue = method.proceed();
+			return returnedValue;
+		} finally {
+			long endTime = System.nanoTime();
+			double timeTaken = (double) (endTime - startTime) / 1000000000.0;
+			System.out.println("The method " + method.getSignature().getName() 
+					+ " took " + timeTaken + " seconds");
+		}
+	}
+
+	public void beforeAdviceTesting(JoinPoint jp)
+	{
+		//BookService b = (BookService)jp.getTarget();
+		//System.out.println(b.getEntireCatalogue());
+		System.out.println("Now entering a method . . . " + jp.getSignature());
+	}
+}
